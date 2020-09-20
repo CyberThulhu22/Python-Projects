e4a48fc501332a1f10e5bc802252377dbf7d12de
CyberThulhu22 <37412977+CyberThulhu22@users.noreply.github.com>
2020-09-20 14:12:22 -0400

    Final Updates to Initial Project
    Finished code to working order. Still need to troubleshoot a few things, and add comments, etc.

--- Network/IP_Subnet_List/test_1_subnet.py
+++ Network/IP_Subnet_List/test_1_subnet.py
@@ -7,14 +7,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-iL', '--iplist', metavar='<IPv4 List>', required=True, help='Input IPv4 Address List (Each IP needs to be on its own line)')
-parser.add_argument('-sL', '--subnetlist', metavar='<Subnet List>', required=True, help='')
-parser.add_argument('-oF', '--outfile', metavar='<Outfile>', help='')
+parser.add_argument('-sL', '--subnetlist', metavar='<Subnet List>', required=True, help='Input Subnet List (Each Subnet IP, needs to be on its own line)')
+parser.add_argument('-oF', '--outfile', metavar='<Outfile>', help='Outputs results to a file *Option not implemented yet')
 group = parser.add_mutually_exclusive_group()
-group.add_argument('-q', '--quiet', action='store_true', help='Prints quiet')
-group.add_argument('-v', '--v', action='store_true', help='Prints Verbose')
+group.add_argument('-q', '--quiet', action='store_true', help='Prints quiet *Option not implemented yet')
+group.add_argument('-v', '--v', action='store_true', help='Prints Verbose *Option not implemented yet')
 args = parser.parse_args()
-
-output = dict()
 
 ## Defined Functions ##
 def readFile(file):
@@ -24,43 +22,66 @@ def readFile(file):
             outfile.append(line.rstrip())
     return outfile
 
+def outFile():
+    # Create function to take the output and send to a formated file such as txt.
+    pass
+
 def spltIPs(ip):
     return [int(x) for x in ip.split(".")]
 
-def netMask(n=1):
+def netMask(x = 1):
     netmList = list()
-    if n == 1:
-        for each in range(1, 33):
-            netmList.append([( ((1<<32)-1) << (32-each) >> i ) & 255 for i in reversed(range(0, 32, 8))])
+    if x == 1:
+        for cidr in range(1, 33):
+            netmList.append([( ((1<<32)-1) << (32-cidr) >> i ) & 255 for i in reversed(range(0, 32, 8))])
         return netmList
-    else:
-        return netmList.append([( ((1<<32)-1) << (32-each) >> i ) & 255 for i in reversed(range(0, 32, 8))])
+    elif x == 2:
+        return [( ((1<<32)-1) << (32-24) >> i ) & 255 for i in reversed(range(0, 32, 8))]
 
 def netWork(ipaddr, netmask):
     return [ipaddr[i] & netmask[i] for i in range(4)]
 
+def outPut():
+    print("SUBNETS \t:\t IPv4s")
+    for key, item in sorted(output.items()):
+        print(key, "\t:\t", item)
+
 ## Main Prog ##
 if __name__=="__main__":
-    for ipv4 in readFile(args.iplist):
-        outputlist = list()
-        print("[+] " + ipv4 )
-        for nmask in netMask(1):
-            outputlist.append('.'.join(map(str, netWork(spltIPs(ipv4), nmask))))
-        for subn in readFile(args.subnetlist):
-            for each in outputlist:
-                if each == subn:
-                    print("Subnet Found\t" + each)
-                    if each not in output:
-                        output.setdefault(subn, [])
-                        output[subn].append(ipv4)
-                    else:
-                        output.setdefault(subn, []).append(ipv4)
-                if each != subn:
-                    print("Subnet Not Found")
-                    if each not in output:
-                        #output.setdefault()
-                        print()
 
-    print('Subnet\t\t:\t IPv4s')            
-    for key, item in output.items():
-        print(key, '\t:\t', item)
+    output = dict()
+    IPlist = readFile(args.iplist)
+    SUBlist = readFile(args.subnetlist)
+    
+    for ip in IPlist:
+        masklist = list()
+        counter = 0
+        
+        for nm in netMask(1):
+            masklist.append('.'.join(map(str, netWork(spltIPs(ip), nm))))
+        
+        while counter != len(SUBlist):
+            for sub in SUBlist:
+                
+                if any(masklist) == sub and sub in output:
+                    output.setdefault(sub, [])
+                    output[sub].append(ip)
+                    counter = len(SUBlist)
+                    break
+                elif any(masklist) == sub:
+                    output.setdefault(sub, [])
+                    output[sub].append(ip)
+                    counter = len(SUBlist)
+                    break
+                else:
+                    addnew = '.'.join(map(str, netWork(spltIPs(ip), netMask(2))))
+                    output.setdefault(addnew,[])
+                    output[addnew].append(ip)
+                    counter = len(SUBlist)
+                    break
+    for sub in SUBlist:
+        if sub not in output:
+            output.setdefault(sub, [])
+            output[sub].append('')
+
+    print(outPut())
