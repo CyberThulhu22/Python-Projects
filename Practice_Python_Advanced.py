cc87254ce109aa44f3e722a26df9f1db9247cada
CyberThulhu22 <37412977+CyberThulhu22@users.noreply.github.com>
2021-10-15 22:44:20 -0400

    Update Practice_Python_Advanced.py

--- Practice_Python_Advanced.py
+++ Practice_Python_Advanced.py
@@ -8,11 +8,14 @@ TO-DO:
 """
 
 # Imports
+import sys
 import argparse
 import getpass
+from urllib import request, error
 
 # Argparse
 parser = argparse.ArgumentParser(description="")
+parser.add_argument('-i', "--ipaddr", metavar='abc123', default="8.8.8.8", type=str, help="")
 parser.add_argument('-a', "--apikey", metavar='abc123', nargs="?",default=None, type=str, help="")
 args = parser.parse_args()
 
@@ -20,7 +23,18 @@ args = parser.parse_args()
 def get_apikey():
     return getpass.getpass("API_KEY >")
 
+def test_connection(apikey=args.apikey, ipaddr=args.ipaddr):
+    try:
+        status_code = request.urlopen(f"https://api.ipgeolocation.io/ipgeo?apiKey={apikey}&ip={ipaddr}").getcode()
+        if status_code == 200:
+            return status_code
+    except error.HTTPError:
+        print(f"[-] ERROR (HTTPErr: {error.HTTPError().code})")
+        sys.exit(1)
+    
+
 if __name__ == "__main__":
     if args.apikey == None: API_KEY = get_apikey()
     else: API_KEY = args.apikey
     print(API_KEY)
+    print(test_connection())
